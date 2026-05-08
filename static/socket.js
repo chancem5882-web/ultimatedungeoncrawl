f3d70eeab07d15cc33a1895dcb9df8c292606675
chancem5882-web <chancem5882@gmail.com>
2026-05-08 12:26:51 -0500

    Update socket.js

--- static/socket.js
+++ static/socket.js
@@ -1,1 +1,13 @@
+const socket = io();
 
+socket.on("character_updated", ()=>{
+location.reload();
+});
+
+socket.on("new_message", ()=>{
+location.reload();
+});
+
+socket.on("effect_added", ()=>{
+location.reload();
+})
