312b5dafd70c9e71edf1ad9e4cc3f6d37940da6a
chancem5882-web <chancem5882@gmail.com>
2026-05-08 12:25:18 -0500

    Update dice.py

--- systems/dice.py
+++ systems/dice.py
@@ -1,1 +1,15 @@
+import random
 
+
+def d20():
+    return random.randint(1, 20)
+
+
+def roll(number, sides):
+
+    total = 0
+
+    for i in range(number):
+        total += random.randint(1, sides)
+
+    return total
